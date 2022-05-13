09d4eef04a04525fb383a860709d76837fa29cbc
Maliha Mim <maliha1110@gmail.com>
2022-05-14 10:31:24 +1200

    second commit

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import logo from './logo.svg';
+/*import logo from './logo.svg';
 import './App.css';
 
 function App() {
@@ -22,4 +22,32 @@ function App() {
   );
 }
 
+export default App;*/
+
+import React from "react";
+
+import Home from "./pages/Home";
+import SEPractice from "./pages/SE-Practice";
+import SubmitArticle from "./pages/Submit-Article"; 
+import NotFoundPage from "./pages/404";
+
+const App = () =>  {
+    return (
+        <Router>
+        <div>
+          <h1>Software Engineering Practice Evidence Repository (SEPER)</h1>
+            <ul className="header">
+                <li><a href = "/">Home</a></li>
+                <li><a href = "/SEPractice">Select the Practice</a></li>
+                <li><a href = "/SubmitArticle">Submit an Article</a></li>
+            </ul>
+          <div className="content">
+            <
+          </div>
+        </div>
+        </Router>
+    );
+}
+ 
 export default App;
+
