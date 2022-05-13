b4d7d6450f629fed08e58e0f724c3ed49e8b3f42
Maliha Mim <maliha1110@gmail.com>
2022-05-14 11:47:15 +1200

    another commit

--- src/App.js
+++ src/App.js
@@ -1,61 +1,34 @@
-/*import logo from './logo.svg';
-import './App.css';
-
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
-}
-
-export default App;*/
+import React from "react";
 import {
   Route,
   NavLink,
-  BrowserRouter as Router
+  BrowserRouter as Router,
+  Navigate
 } from "react-router-dom";
-
-import React from "react";
 
 import Home from "./pages/Home";
 import SEPractice from "./pages/SE-Practice";
 import SubmitArticle from "./pages/Submit-Article"; 
 import NotFoundPage from "./pages/404";
-
 const App = () =>  {
     return (
         <Router>
         <div>
           <h1>Software Engineering Practice Evidence Repository (SEPER)</h1>
             <ul className="header">
-              <li><NavLink exact to = "/">Home</NavLink></li>
-              <li><NavLink to = "/SEPractice">Select the Practice</NavLink></li>
-              <li><NavLink to = "/SubmitArticle">Submit an Article</NavLink></li>
-
+                <li><NavLink exact to = "/">Home</NavLink></li>
+                <li><NavLink to = "/SEPractice">Select the Practice</NavLink></li>
+                <li><NavLink to = "/SubmitArticle">Submit an Article</NavLink></li>
             </ul>
           <div className="content">
-              <li><NavLink exact to = "/">Home</NavLink></li>
-              <li><NavLink to = "/SEPractice">Select the Practice</NavLink></li>
-              <li><NavLink to = "/SubmitArticle">Submit an Article</NavLink></li>
+            <Route exact path="/" component={Home}/>
+            <Route  path="/SEPractice" component={SEPractice}/>
+            <Route  path="/SubmitArticle" component={SubmitArticle}/>
+            <Route exact path="/404" component={NotFoundPage}/>
+            <Navigate to="/404" />
           </div>
         </div>
         </Router>
     );
 }
- 
 export default App;
-
